646262867aa7df28b297ec0ec6fa10ac65c2535c
thiago <thiago_arruda@id.uff.br>
2023-10-23 18:05:34 -0300

    Popular and veggies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Pages from './pages/Pages';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages />
+    </div>
+  );
+}
+
+export default App;
